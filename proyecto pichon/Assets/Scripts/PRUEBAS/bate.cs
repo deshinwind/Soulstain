2ed11400c71f63e47fd4988c3bb8a7f038f1c9ed
JuananEPCesur <150778772+JuananEPCesur@users.noreply.github.com>
2024-02-14 12:18:20 +0100

    bate

--- proyecto pichon/Assets/Scripts/PRUEBAS/bate.cs
+++ proyecto pichon/Assets/Scripts/PRUEBAS/bate.cs
@@ -7,6 +7,16 @@ public class bate : MonoBehaviour
     public Material mesatext;
     public bool isgrab = false;
     public GameObject mesarota;
+    public float fadeDuration = 2f; // Duración de la animación de desvanecimiento
+    private float fadeTimer = 0f;
+    private bool isBroken = false;
+
+    private void Start()
+    {
+        Color color = mesatext.color;
+        color.a = 1;
+        mesatext.color = color;
+    }
     public void agarrao()
     {
         isgrab = true;
@@ -17,13 +27,30 @@ public class bate : MonoBehaviour
         isgrab=false;
     }
 
+    private void Update()
+    {
+        if (fadeTimer < fadeDuration && isBroken)
+        {
+            // Incrementa el temporizador de desvanecimiento
+            fadeTimer += Time.deltaTime;
+            // Calcula la nueva transparencia basada en el tiempo transcurrido
+            float alpha = 1 - (fadeTimer / fadeDuration);
+            // Establece la transparencia en el material
+            Color color = mesatext.color;
+            color.a = alpha;
+            mesatext.color = color;
+        }
+    }
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "mesa" && isgrab)
         {
+            isBroken = true;
             mesarota = Instantiate(mesarota, collision.transform.position, mesarota.transform.rotation);
+            Destroy(mesarota, fadeDuration-1);
             Destroy(collision.gameObject);
-            Destroy(mesarota, 3f);
         }
     }
 
